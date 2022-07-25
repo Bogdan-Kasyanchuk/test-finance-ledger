87a1b7d41001ebd77d476af959218a07af451c88
Bogdan-Kasyanchuk <bogdan.k1990@gmail.com>
2022-07-25 23:13:31 +0300

    add basic-settings

---
+++ src/sections/ContactsPage/ContactsPage.js
@@ -0,0 +1,15 @@
+import Section from 'components/Section/Section';
+import Container from 'components/Container/Container';
+import Contacts from 'components/Contacts/Contacts';
+
+const ContactsPage = () => {
+  return (
+    <Section>
+      <Container>
+        <Contacts />
+      </Container>
+    </Section>
+  );
+};
+
+export default ContactsPage;
---
+++ src/styles/variables.js
@@ -0,0 +1,40 @@
+// colors
+const firstColor = '#333';
+const secondColor = '#fff';
+const thirdColor = '#e9e9e9';
+const fourthColor = '#808080';
+const accentColor = '#28a745';
+const errorColor = '#ce0a0a';
+const backgroundColor = '#0284d0';
+
+// border
+const borderRadius = '5px';
+const border = '1px solid';
+
+// timingFunc;
+const duration = '250ms';
+const timing = 'cubic-bezier(0.4, 0, 0.2, 1)';
+
+//breakpoints
+const size = {
+  mobileMin: '@media screen and (min-width: 320px)',
+  mobileMax: '@media screen and (max-width: 767px)',
+  tabletMin: '@media screen and (min-width: 768px)',
+  tabletMax: '@media screen and (max-width: 1359px)',
+  desktopMin: '@media screen and (min-width: 1360px)',
+};
+
+export {
+  size,
+  duration,
+  timing,
+  firstColor,
+  secondColor,
+  thirdColor,
+  fourthColor,
+  accentColor,
+  errorColor,
+  backgroundColor,
+  borderRadius,
+  border,
+};
